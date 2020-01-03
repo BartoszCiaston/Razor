ef3c1ada408c1b9ddcd9dcb1f67e098d7ecdb14c
ciacho <bartoszciaston1@gmail.com>
2020-01-03 15:58:02 +0100

    Use array in HomeController for products

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -9,18 +9,16 @@ namespace Razor.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        public IActionResult Index()
         {
-            Product myProduct = new Product
+            Product[] array =
             {
-                ProductID = 1,
-                Name = "Kajak",
-                Description = "Jednoosobowa łódka",
-                Category = "Sporty wodne",
-                Price = 275M
+                new Product {Name = "Kajak", Price = 275M},
+                new Product {Name = "Kamizelka ratunkowa", Price = 48.95M},
+                new Product {Name = "Piłka nożna", Price = 19.50M},
+                new Product {Name = "Flaga narożna", Price= 34.95M}
             };
-            ViewBag.StockLevel = 2;
-            return View(myProduct);
+            return View(array);
         }
     }
 }
